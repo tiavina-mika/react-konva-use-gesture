d8ce4ed66f046387a7210db316a898ab0e2d38d7
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-11-29 10:18:22 +0100

    fix move

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { render } from 'react-dom'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Stage, Layer, Circle, Rect } from 'react-konva'
 import './styles.css'
 import { createUseGesture, dragAction, pinchAction } from '@use-gesture/react'
@@ -16,53 +16,39 @@ function Drag() {
   })
   const ref = React.useRef(null)
 
-  useEffect(() => {
-    const handler = (e) => e.preventDefault()
-    document.addEventListener('gesturestart', handler)
-    document.addEventListener('gesturechange', handler)
-    return () => {
-      document.removeEventListener('gesturestart', handler)
-      document.removeEventListener('gesturechange', handler)
-    }
-  }, [])
-
   useGesture(
     {
       // onHover: ({ active, event }) => console.log('hover', event, active),
       // onMove: ({ event }) => console.log('move', event),
       onDrag: ({ pinching, cancel, offset: [x, y], ...rest }) => {
-        // if (pinching) return cancel()
-        setValues({ imageLeft: x, imageTop: y, ...values })
+        if (pinching) return cancel()
+        setValues({ imageLeft: x, imageTop: y })
         // api.start({ x, y })
+      },
+      onPinch: ({ origin: [ox, oy], first, movement: [ms], offset: [s, a], memo }) => {
+        if (first) {
+          const rect = ref.current
+          const x = rect.x()
+          const y = rect.y()
+          const width = rect.width()
+          const height = rect.height()
+          // const { width, height, x, y } = ref.current.getBoundingClientRect()
+          const tx = ox - (x + width / 2)
+          const ty = oy - (y + height / 2)
+          memo = [values.imageLeft, values.imageTop, tx, ty]
+        }
+
+        const x = memo[0] - ms * memo[2]
+        const y = memo[1] - ms * memo[3]
+        setValues({
+          imageLeft: x,
+          imageTop: y,
+          imageWidth: values.imageWidth * s,
+          imageHeight: values.imageHeight * s
+        })
+
+        return memo
       }
-      // onPinch: ({ dragging, cancel, origin: [ox, oy], first, movement: [ms], offset: [s, a], memo }) => {
-      //   if (dragging) return cancel()
-      //   if (first) {
-      //     const rect = ref.current
-      //     const x = rect.x()
-      //     const y = rect.y()
-      //     const width = rect.width()
-      //     const height = rect.height()
-      //     // const { width, height, x, y } = ref.current.getBoundingClientRect()
-      //     const tx = ox - (x + width / 2)
-      //     const ty = oy - (y + height / 2)
-      //     memo = [values.imageLeft, values.imageTop, tx, ty]
-      //   }
-
-      //   const x = memo[0] - ms * memo[2]
-      //   const y = memo[1] - ms * memo[3]
-      //   console.log('s', s)
-
-      //   setValues({
-      //     ...values,
-      //     imageLeft: x,
-      //     imageTop: y,
-      //     imageWidth: values.imageWidth * s,
-      //     imageHeight: values.imageHeight * s
-      //   })
-
-      //   return memo
-      // }
     },
     {
       target: ref,
@@ -93,8 +79,7 @@ function Drag() {
         </Layer>
       </Stage>
       <h1>
-        x: {Math.round(values.imageLeft)}, y: {Math.round(values.imageTop)}, width: {Math.round(values.imageWidth)},
-        height: {Math.round(values.imageHeight)},
+        x: {Math.round(values.imageLeft)}, y: {Math.round(values.imageTop)},
       </h1>
     </div>
   )
