2d6e960f4299cd418d91e4d3a498209a055679fa
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-11-29 09:07:16 +0100

    update rect size

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { render } from 'react-dom'
 import React, { useState } from 'react'
-import { Stage, Layer, Circle } from 'react-konva'
+import { Stage, Layer, Circle, Rect } from 'react-konva'
 import './styles.css'
 import { createUseGesture, dragAction, pinchAction } from '@use-gesture/react'
 
@@ -9,7 +9,9 @@ const useGesture = createUseGesture([dragAction, pinchAction])
 function Drag() {
   const [values, setValues] = useState({
     imageLeft: 0,
-    imageTop: 0
+    imageTop: 0,
+    imageWidth: 100,
+    imageHeight: 100
   })
   const ref = React.useRef(null)
 
@@ -24,7 +26,12 @@ function Drag() {
       },
       onPinch: ({ origin: [ox, oy], first, movement: [ms], offset: [s, a], memo }) => {
         if (first) {
-          const { width, height, x, y } = ref.current.getBoundingClientRect()
+          const rect = ref.current
+          const x = rect.x()
+          const y = rect.y()
+          const width = rect.width()
+          const height = rect.height()
+          // const { width, height, x, y } = ref.current.getBoundingClientRect()
           const tx = ox - (x + width / 2)
           const ty = oy - (y + height / 2)
           memo = [values.imageLeft, values.imageTop, tx, ty]
@@ -47,7 +54,7 @@ function Drag() {
     <div className="container">
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Circle
+          <Rect
             ref={ref}
             // onMouseDown={e => {
             //   props.onMouseDown(e.evt)
@@ -57,7 +64,9 @@ function Drag() {
             // }}
             x={values.imageLeft}
             y={values.imageTop}
-            radius={100}
+            width={100}
+            height={100}
+            // radius={100}
             fill="red"
           />
         </Layer>
