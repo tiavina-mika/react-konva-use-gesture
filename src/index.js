5624ec5f632299ef754adf5eec9dca7e3b1aed2e
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-11-29 10:20:00 +0100

    change values

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function Drag() {
       // onMove: ({ event }) => console.log('move', event),
       onDrag: ({ pinching, cancel, offset: [x, y], ...rest }) => {
         if (pinching) return cancel()
-        setValues({ imageLeft: x, imageTop: y })
+        setValues({ imageLeft: x, imageTop: y, ...values })
         // api.start({ x, y })
       },
       onPinch: ({ origin: [ox, oy], first, movement: [ms], offset: [s, a], memo }) => {
